f9c5a2135dbf7ab7bd614c24f6ccc46b3c9653d7
maksym.kamyshnikov <maksym.kamyshnikov@ainstainer.de>
2016-05-12 19:28:17 +0300

    123

--- DashboardExtensions/app.js
+++ DashboardExtensions/app.js
@@ -5,7 +5,6 @@ require(['DashboardSourcesViewModel', 'WidgetSettingsEditorRegistry', 'FreeTextE
         });
     };
 
-    console.log("test");
     loadTemplate('FreeTextTemplate');
     DashboardSourcesViewModel.getSingleton().addMiscellaneousSource(FreeTextExtension.options);
     WidgetSettingsEditorRegistry.getSingleton().registerEditor(FreeTextExtension.type, FreeTextExtension.editor);
